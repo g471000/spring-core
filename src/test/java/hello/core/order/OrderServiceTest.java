408c23c661b1025336e40d5f8a4c74cda27a959b
JIEUN CHON <g471000@gmail.com>
2021-12-28 21:47:50 +0900

    finished junit test for the order service

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "pikachu", Grade.VIP);
+        memberService.join(member);
+
+        String itemName = "pokeball";
+
+        Order order = orderService.createOrder(memberId, itemName, 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+        assertThat(order.getItemName()).isEqualTo(itemName);
+        assertThat(order.getItemPrice()).isEqualTo(10000);
+    }
+}
