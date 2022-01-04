f02f6b384003c51dbd42c4a7a05853350dbf78d4
geniechon <genie@tappytoon.com>
2022-01-04 21:03:52 +0900

    find beans with name or type

---
+++ src/test/java/hello/core/beanfiner/ApplicationContextBasicFindTest.java
@@ -0,0 +1,37 @@
+package hello.core.beanfiner;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("Find with bean name")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("Find with bean type")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("Find bean with non exist name")
+    void findBeanByNonExistName() {
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("heize", MemberService.class));
+    }
+}
